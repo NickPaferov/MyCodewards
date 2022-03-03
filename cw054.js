ccfe817dee676fe3eb267ce0ae4a9fac443093d9
NickPaferov <paferov.nikolai@gmail.com>
2022-03-03 23:12:25 +0300

    Find Maximum and Minimum Values of a List

---
+++ cw054.js
@@ -0,0 +1,13 @@
+const min = (list) => Math.min(...list)
+const max = (list) => Math.max(...list)
+
+/*
+const min = (list) => {
+    list.sort((a, b) => (a - b))
+    return list[0]
+}
+
+const max = (list)=>{
+    list.sort((a, b) => (b - a))
+    return list[0]
+}*/
