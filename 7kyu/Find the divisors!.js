538d7e4c6aa4fc33ae1e142feddf91dcd9152490
Paferov Nikolai <paferov.nikolai@gmail.com>
2022-08-20 19:44:35 +0300

    7kyu_016

---
+++ 7kyu/Find the divisors!.js
@@ -0,0 +1,9 @@
+const divisors = integer => {
+    let arr = []
+    for (let i = 2; i < integer; i++) {
+        if (integer % i === 0) {
+            arr.push(i)
+        }
+    }
+    return arr.length ? arr : `${integer} is prime`
+}
