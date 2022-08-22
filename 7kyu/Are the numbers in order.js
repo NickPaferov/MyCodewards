7e6680bf029761841c84d984a10c5f695f1ac595
Paferov Nikolai <paferov.nikolai@gmail.com>
2022-08-22 11:18:03 +0300

    7kyu_019

---
+++ 7kyu/Are the numbers in order.js
@@ -0,0 +1,10 @@
+/*const inAscOrder = arr => {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (!(arr[i] < arr[i + 1])) {
+            return false
+        }
+    }
+    return true
+}*/
+
+const inAscOrder = arr => arr.join("") === arr.sort((a, b) => a - b).join("")
