6073b3d71c6bc5aaa080a54b1e2ef62fe524eae6
Paferov Nikolai <paferov.nikolai@gmail.com>
2022-09-21 13:57:47 +0300

    7kyu_065

---
+++ 7kyu/Averages of numbers.js
@@ -0,0 +1,9 @@
+const averages = numbers => {
+    let arr = []
+    if (numbers) {
+        for (let i = 0; i < numbers.length - 1; i++) {
+            arr.push((numbers[i] + numbers[i + 1]) / 2)
+        }
+    }
+    return arr
+}
