4c20c0c7f27eae6251a99481f66fad8c4395763a
Paferov Nikolai <paferov.nikolai@gmail.com>
2022-10-13 12:06:40 +0300

    7kyu_094

---
+++ 7kyu/Lost number in number sequence.js
@@ -0,0 +1,8 @@
+const findDeletedNumber = (arr, mixArr) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (!mixArr.includes(arr[i])) {
+            return arr[i]
+        }
+    }
+    return 0
+}
